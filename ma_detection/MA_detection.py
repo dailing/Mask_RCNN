c6676084d119129fef01155c5c0146e3d79a4899
dailing <qzyz_dailing@163.com>
2019-06-05 13:24:44 +0800

    shrinked the archor box,

--- ma_detection/MA_detection.py
+++ ma_detection/MA_detection.py
@@ -9,8 +9,12 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
+import random
 from PIL import Image
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from torch.optim import SGD
+from tqdm import tqdm
+from tensorboardX import SummaryWriter
 
 from augment import FundusAOICrop, CompostImageAndLabel
 from model import mean_iou, Mrcnn
@@ -38,8 +42,8 @@ class VGG(nn.Module):
         self.rpn_sliding_window = nn.Conv2d(
             512, 256, 3, 1, 1
         )
-        self.box_classification = nn.Conv2d(256, 2 * 15, 1)
-        self.box_regression = nn.Conv2d(256, 4 * 15, 1)
+        self.box_classification = nn.Conv2d(256, 2 * 1, 1)
+        self.box_regression = nn.Conv2d(256, 2 * 1, 1)
         if init_weights:
             self._initialize_weights()
 
@@ -221,26 +225,14 @@ class BBloader(Dataset):
         self.split = split
         if archers is None:
             archers = [
-                (7, 7),
-                (15, 15),
                 (30, 30),
-                (30, 60),
-                (60, 30),
-                (60, 60),
-                (120, 60),
-                (60, 120),
-                (120, 120),
-                (240, 120),
-                (120, 240),
-                (240, 240),
-                (480, 240),
-                (240, 480),
-                (480, 480),
             ]
         self.archers = archers
         self.n_archer = len(self.archers)
         self.ratio = 16
         self.file_list = self._make_slices()
+        self.thresh = 0.99
+        self.smooth_factor = 100000
 
     def _make_slices(self):
         store_dir = f'runs/fundus_image_data/{self.split}'
@@ -292,14 +284,13 @@ class BBloader(Dataset):
         nchannel, nrow, ncol = image.shape
 
         arow, acol = nrow // self.ratio, ncol // self.ratio
-        archor_reg = np.zeros((self.n_archer, 4, arow, acol), np.float32)
+        archor_reg = np.zeros((self.n_archer, 2, arow, acol), np.float32)
         positive = np.zeros((self.n_archer, 1, arow, acol), np.int)
         negative = np.zeros((self.n_archer, 1, arow, acol), np.int)
         arc_to_bbox_map = np.zeros((self.n_archer, 1, arow, acol), np.int) - 1
 
         center_rows = [self.ratio // 2 + i * self.ratio for i in range(arow)]
         center_cols = [self.ratio // 2 + i * self.ratio for i in range(acol)]
-
         for bbox_idx, label_box in enumerate(bbox):
             if label_box[-1] != 1:
                 continue
@@ -314,60 +305,166 @@ class BBloader(Dataset):
                     *self.archers[iarc]
                 )
                 iou_map[iarc, 0, irow, icol] = mean_iou(abox, label_box)
-            if np.all(iou_map < 0.5):
-                h_thresh = np.max(iou_map)
-                l_thresh = 0.5 * h_thresh
-            else:
-                h_thresh = 0.5
-                l_thresh = 0.3
-            positive += iou_map >= h_thresh
-            negative += iou_map < l_thresh
+            h_thresh = 0
+            l_thresh = 0
+            positive += iou_map > h_thresh
+            negative += iou_map <= l_thresh
             arc_to_bbox_map[iou_map >= h_thresh] = bbox_idx
+        positive = positive > 0
+        positive = positive.astype(np.float32)
+        negative = negative > 0
+        negative = negative.astype(np.float32)
 
         n_postive = np.sum(positive)
-        negative_points = np.where(negative)
-        indices = np.random.choice(
-            np.arange(negative_points[0].size),
-            replace=False,
-            size=max(0, negative_points[0].size - n_postive)
-        )
-        sss = tuple((i[indices] for i in negative_points))
-        negative[sss] = 0
-        train_mask = positive | negative
+        mask = positive > 0
+        random_sample = np.random.rand(*mask.shape)
+        mask += random_sample > self.thresh
+        mask = mask > 0
+        mask = mask.astype(np.float32)
+        n_psample = np.sum((mask * positive) > 0)
+        n_nsample = np.sum((mask * negative) > 0)
+        self.thresh += n_psample / self.smooth_factor
+        self.thresh -= n_nsample / self.smooth_factor
+
         archor_cls = np.concatenate(
             (negative.astype(np.int), positive.astype(np.int)),
             axis=1)
+
+        # logger.info(f'{arow}, {acol}, {self.n_archer}, {len(bbox)}')
         for irow, icol, iarc in product(
                 range(arow),
                 range(acol),
                 range(self.n_archer)):
-            if not train_mask[iarc, 0, irow, icol]:
+            if positive[iarc, 0, irow, icol] == 0:
                 continue
             current_bbox = bbox[arc_to_bbox_map[iarc, 0, irow, icol]]
-            t_row = (current_bbox[0] - center_rows[irow]) / self.archers[iarc][0]
-            t_col = (current_bbox[1] - center_cols[icol]) / self.archers[iarc][1]
-            t_row_len = math.log(current_bbox[2] / self.archers[iarc][0])
-            t_col_len = math.log(current_bbox[3] / self.archers[iarc][1])
+            t_row = (current_bbox[0] - center_rows[irow])\
+                / self.archers[iarc][0]
+            t_col = (current_bbox[1] - center_cols[icol])\
+                / self.archers[iarc][1]
             archor_reg[iarc, :, irow, icol] = (
-                t_row,
-                t_col,
-                t_row_len,
-                t_col_len)
+                t_row, t_col)
         archor_cls = archor_cls.astype(np.float32)
-        train_mask = train_mask.astype(np.float32)
-        return image, (archor_cls, archor_reg, train_mask)
+        return image, (archor_cls, archor_reg, mask)
 
     def __len__(self):
         return self.file_list.__len__()
 
 
+class MaDetector:
+    def __init__(
+            self,
+            device=None,
+            model=None,
+            train_data=None,
+            test_data=None,
+            ratio=None,
+            net=None):
+        self.device = torch.device(device) if device else torch.device('cpu')
+        self.net = VGG() if net is None else net
+        if model is not None:
+            logger.info(f'loading form {model}')
+            dd = torch.load(model)
+            self.net.load_state_dict(dd)
+        self.net.to(self.device)
+        self.epoach = 0
+        self.train_data = BBloader('train', [(30, 30)]) 
+        self.test_data = BBloader('train', [(30, 30)])
+        if ratio is None:
+            self.ratio = self.train_data.ratio
+        else:
+            self.ratio = ratio
+        self.train_loader = DataLoader(
+            self.train_data,
+            3,
+            True,
+            num_workers=5)
+        self.optm = SGD(
+            self.net.parameters(),
+            lr=0.0003,
+            momentum=0.9,
+            nesterov=True,
+            weight_decay=0.00005
+        )
+        self.logWriter = SummaryWriter(logdir=f'log/fuck')
+
+    def train(self):
+        self.net.train()
+        tt = tqdm(self.train_loader, total=len(self.train_loader))
+        for idx, (img, (cls, reg, mask)) in enumerate(tt):
+            npmask = mask.numpy()
+            if npmask.sum() == 0:
+                continue
+            mask = mask.to(self.device)
+
+            img = img.to(self.device)
+            cls = cls.to(self.device)
+            reg = reg.to(self.device)
+            mask = mask.to(self.device)
+            pcls, preg = self.net(img)
+
+            # Calculate Loss_cls
+            pcls = pcls.reshape(cls.shape)
+            pcls = torch.nn.functional.log_softmax(pcls, dim=2)
+            pcls = pcls * cls
+            pcls = pcls * mask
+            L_cls = - pcls.sum() / mask.sum()
+
+            # Calculate Loss_reg
+            preg = preg.reshape(reg.shape)
+            L_reg = torch.abs(preg - reg)
+            L_reg = torch.where(L_reg < 1, 0.5 * L_reg ** 2, L_reg - 0.5)
+            positive = cls[:, :, 1:, :, :]
+            positive_sum = positive.sum()
+            L_reg = L_reg * positive
+            L_reg = L_reg.sum() / positive_sum
+
+            self.optm.zero_grad()
+            loss = L_cls + (0.1 * L_reg if positive_sum > 0 else 0)
+            loss.backward()
+            self.optm.step()
+
+            loss = loss.detach().cpu()
+            tt.set_postfix_str(str(loss))
+            self.logWriter.add_scalar(
+                'train/loss',
+                float(loss),
+                (self.epoach - 1) * len(self.train_loader) + idx)
+
+    def step(self, n=300):
+        for i in tqdm(range(n), total=n):
+            self.epoach += 1
+            self.train()
+
 if __name__ == '__main__':
-    det = Mrcnn(
-        train_data=BBloader(split='train'),
+
+    detector = MaDetector(
+        device='cuda',
         net=VGG(),
-        model='runs/model_0193.model'
     )
-    # det.step()
+    detector.step()
+    sys.exit(0)
+
+    loader = BBloader(split='train')
+
+    for i in range(100):
+        idx = random.randint(0, len(loader))
+        iimg, (classification, regression, mask) = loader[idx]
+        logger.info(iimg.shape)
+
+        plt.figure()
+        plt.imshow(classification[0, 1, :, :])
+        plt.colorbar()
+
+        plt.figure()
+        plt.imshow(iimg.transpose(1, 2, 0))
+
+        plt.figure()
+        plt.imshow(mask[0, 0, ::])
+        plt.show()
+    sys.exit(0)
+
+    logger.info(iimg.shape)
 
     image = Image.open('/data/home/d/data/challenge/A. Segmentation/'
                        '1. Original Images/a. Training Set/IDRiD_34.jpg')
@@ -375,8 +472,8 @@ if __name__ == '__main__':
     imgs, pos = slide_image(image, 512, 0.2)
     result_patchs = []
 
-    heatmap = np.zeros((15, image.shape[0]//16, image.shape[1]//16))
-    counter = np.zeros((15, image.shape[0]//16, image.shape[1]//16))
+    heatmap = np.zeros((1, image.shape[0]//16, image.shape[1]//16))
+    counter = np.zeros((1, image.shape[0]//16, image.shape[1]//16))
     for img_patchs, p in zip(imgs, pos):
         cls, reg = det.predict(img_patchs)
         p = tuple(i // 16 for i in p)
